04e83f1c92afb98cc4528a48c70b4324820b5b5e
empir <empiredbdr@163.com>
2024-04-02 23:36:58 +0100

    0402.

--- src/main/java/com/ucas/nabla/controller/TmDataHandler.java
+++ src/main/java/com/ucas/nabla/controller/TmDataHandler.java
@@ -1,8 +1,11 @@
 package com.ucas.nabla.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ucas.nabla.entity.TmData;
 import com.ucas.nabla.repository.TmDataRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
--- src/main/java/com/ucas/nabla/controller/gateController.java
+++ src/main/java/com/ucas/nabla/controller/gateController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class gateController {
 
-    @GetMapping("/to_upload")
+    @GetMapping("/nabla")
     public String toGate(){
         return "file-upload";
     }
